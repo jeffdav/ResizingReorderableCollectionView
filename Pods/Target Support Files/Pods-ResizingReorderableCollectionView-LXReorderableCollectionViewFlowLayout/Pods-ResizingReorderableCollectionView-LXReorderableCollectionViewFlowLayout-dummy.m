80fe2e3be4022312971ad229be8df86003337f43
Eric Jensen <jensen39@gmail.com>
2014-10-28 17:13:06 -0400

    Added pod folder so the project can be built and run without using cocoa pods first

---
+++ Pods/Target Support Files/Pods-ResizingReorderableCollectionView-LXReorderableCollectionViewFlowLayout/Pods-ResizingReorderableCollectionView-LXReorderableCollectionViewFlowLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResizingReorderableCollectionView_LXReorderableCollectionViewFlowLayout : NSObject
+@end
+@implementation PodsDummy_Pods_ResizingReorderableCollectionView_LXReorderableCollectionViewFlowLayout
+@end
---
+++ Pods/Target Support Files/Pods-ResizingReorderableCollectionView/Pods-ResizingReorderableCollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResizingReorderableCollectionView : NSObject
+@end
+@implementation PodsDummy_Pods_ResizingReorderableCollectionView
+@end
